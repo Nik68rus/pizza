bf1a400952eebc203ef87fe227d6a6cc0ae23802
Nik68rus <nik200x@mail.ru>
2022-12-25 21:36:28 +0300

    доработки

--- client/src/components/PizzaItem.tsx
+++ client/src/components/PizzaItem.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
+import { FaPlus } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../hooks/store';
 import { addItem, selectCartitem } from '../store/slices/cartSlice';
@@ -14,6 +15,16 @@ const PizzaItem = ({ id, title, price, imageUrl, bases, sizes }: IPizza) => {
   const dispatch = useAppDispatch();
   const cartItem = useAppSelector(selectCartitem(cartItemId));
 
+  const getCurrentPrice = useCallback(() => {
+    if (activeSize === 30) {
+      return Math.round(price * 1.2);
+    } else if (activeSize === 40) {
+      return Math.round(price * 1.5);
+    } else {
+      return price;
+    }
+  }, [price, activeSize]);
+
   const clickHandler = () => {
     dispatch(
       addItem({
@@ -22,7 +33,7 @@ const PizzaItem = ({ id, title, price, imageUrl, bases, sizes }: IPizza) => {
         title,
         base: activeBase,
         size: activeSize,
-        price,
+        price: getCurrentPrice(),
       })
     );
   };
@@ -58,23 +69,12 @@ const PizzaItem = ({ id, title, price, imageUrl, bases, sizes }: IPizza) => {
         </ul>
       </div>
       <div className="pizza-block__bottom">
-        <div className="pizza-block__price">от {price} ₽</div>
+        <div className="pizza-block__price">{getCurrentPrice()} ₽</div>
         <button
           className="button button--outline button--add"
           onClick={clickHandler}
         >
-          <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-              fill="white"
-            />
-          </svg>
+          <FaPlus />
           <span>Добавить</span>
           {cartItem ? <i>{cartItem.qty}</i> : null}
         </button>
